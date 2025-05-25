195af57f7284193abb33753a010fd2733581d30d
Gustavo <gustavoribas.ads@gmail.com>
2025-05-24 21:50:36 -0300

    create list page layout

---
+++ src/app/(private)/list/constants.ts
@@ -0,0 +1,22 @@
+export const headers = [
+    {
+        key: 'local',
+        label: 'Local'
+    },
+    {
+        key: 'date',
+        label: 'Data'
+    },
+    {
+        key: 'name',
+        label: 'Nome'
+    },
+    {
+        key: 'address',
+        label: 'Endereço'
+    },
+    {
+        key: 'actions',
+        label: 'Ações'
+    }
+]
