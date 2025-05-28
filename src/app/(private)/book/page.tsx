00bf9e4a547c96e87152ecc6601804e2ae3a29b3
Gustavo <gustavoribas.ads@gmail.com>
2025-05-28 11:33:20 -0300

    added button

--- src/app/(private)/book/page.tsx
+++ src/app/(private)/book/page.tsx
@@ -1,10 +1,20 @@
 import BookBlock from "@/app/components/BookBlock";
+import Button from "@/app/components/Button";
 
 
 export default function Book() {
     return (
       <main >
-      <BookBlock />
+        <div className="flex justify-end p-4">
+        <Button variant="primary" className="w-48 p-4">
+          <p>
+           Minhas Reservas
+          </p>
+        </Button>
+        </div>
+        <div className="flex justify-start pl-4">
+          <BookBlock />
+        </div>
       </main>
     );
   }
