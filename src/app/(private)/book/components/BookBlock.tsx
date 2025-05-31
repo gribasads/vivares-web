fb31879240f5403d469746aa175da8a9eaecbbae
Gustavo <gustavoribas.ads@gmail.com>
2025-05-31 09:18:57 -0300

    update

--- src/app/(private)/book/components/BookBlock.tsx
+++ src/app/(private)/book/components/BookBlock.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react'
 import books from "@/mocks/books.json";
 import Image from 'next/image';
-import ModalBook from '../(private)/book/components/ModalBook';
+import ModalBook from './ModalBook';
 
 export default function BookBlock() {
     const [selectedBook, setSelectedBook] = useState<string | null>(null);
@@ -28,11 +28,11 @@ export default function BookBlock() {
                     <div 
                         key={book.id} 
                         className="bg-white p-10 rounded-md shadow-md hover:scale-105 transition-all duration-300 cursor-pointer"
-                        onClick={() => handleBookClick(book.name, book.needPaid)}
+                        onClick={() => handleBookClick(book.name, book.needPayment)}
                     >
                         <Image src="/logo/logo.png" alt={book.name} width={100} height={100} />
                         <h1 className="text-xl font-bold">{book.name}</h1>
-                        <p className="text-sm text-[#6b7280]">{book.needPaid && "Necessita pagamento"}</p>
+                        <p className="text-sm text-[#6b7280]">{book.needPayment && "Necessita pagamento"}</p>
                     </div>
                 ))}
             </div>
